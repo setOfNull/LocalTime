ea489bcca605095c29e5374a820db5f6dc830101
SetOfNull <mnurinissa@gmail.com>
2023-04-18 00:28:48 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+
+public class Main {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.of(2009,9,29,15,45,15);
+        System.out.println("1. *** " + localDateTime.format(FORMATTER) + " ***");
+        System.out.println("2.День недели: " + localDateTime.getDayOfWeek());
+        System.out.println("3.День в году: " + localDateTime.getDayOfYear());
+        System.out.println("4.Неделя месяца: " + localDateTime.get(WeekFields.ISO.weekOfMonth()));
+        System.out.println("5.Месяц: " + localDateTime.getMonth());
+    }
+    }
