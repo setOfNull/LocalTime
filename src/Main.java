f1e531ebfa3308cea093c0ddd7ab0e721b33a1e9
SetOfNull <mnurinissa@gmail.com>
2023-04-18 18:11:36 +0600

    some-changes

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.time.temporal.WeekFields;
 public class Main {
     static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
     public static void main(String[] args) {
-        LocalDateTime localDateTime = LocalDateTime.of(2009,9,29,15,45,15);
+        LocalDateTime localDateTime = LocalDateTime.of(2009,9,28,15,45,15);
         System.out.println("1. *** " + localDateTime.format(FORMATTER) + " ***");
         System.out.println("2.День недели: " + localDateTime.getDayOfWeek());
         System.out.println("3.День в году: " + localDateTime.getDayOfYear());
